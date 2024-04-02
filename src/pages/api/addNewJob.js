e904fab43367f7bd45dc3d81fc8f06dc99398777
Antoine <antoine.aubard@gmail.com>
2024-04-02 12:31:15 +0200

    changed path to models

--- src/pages/api/addNewJob.js
+++ src/pages/api/addNewJob.js
@@ -1,5 +1,5 @@
 import { DataStore } from "@aws-amplify/datastore";
-import { JobList } from "../../models";
+import { JobList } from "@/models";
 
 export default async function addNewJob(data) {
   try {
